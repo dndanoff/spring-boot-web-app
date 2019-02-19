bba0501949fed053dd457b6477aaae6019fdcba2
dndanoff <denis_danof@abv.bg>
2019-02-19 23:08:54 +0200

    add frontend

---
+++ team-frontend/src/main/java/com/danoff/ui/SpringFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.danoff.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrontendApplication.class, args);
+	}
+
+}
---
+++ team-frontend/src/main/java/com/danoff/ui/config/ApplicationProperties.java
@@ -0,0 +1,16 @@
+package com.danoff.ui.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class ApplicationProperties {
+	
+	private final Environment env;
+	
+	@Autowired
+	public ApplicationProperties(Environment env) {
+		this.env = env;
+	}
+}
